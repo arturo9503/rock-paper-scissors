50522acf2cee1658f39a00b154c48bd9817c82d0
Arturo <arturo9503@gmail.com>
2022-07-05 17:39:54 -0400

    Add game functions

---
+++ script.js
@@ -0,0 +1,48 @@
+function computerPlay() {
+    let max = 4;
+    let min = 1;
+    selection = Math.floor(Math.random()*(max - min )) + min
+    if (selection == 1){
+        return 'rock';
+    }
+    else if (selection == 2){
+        return 'paper';
+    }
+    else{
+        return 'scissors'
+    }
+}
+
+
+function userPlay() {
+    let selection = window.prompt('What is your selection?');
+    return selection;
+}
+
+let pc = computerPlay();
+console.log(pc);
+let p1 = userPlay();
+
+
+function game(p1, pc){
+    if (p1 == 'rock' && pc == 'scissors'){
+        return 'Player wins!';
+    }
+    else if (p1 == 'paper' && pc == 'rock'){
+        return 'Player wins!';
+    }
+    else if (p1 == 'scissors' && pc == 'paper'){
+        return 'Player wins!';
+    }
+    else if (p1 == pc){
+        return 'Tie!';
+    }
+    else{
+        return 'PC wins!'
+    }
+}
+
+console.log(game(p1, pc));
+
+
+
