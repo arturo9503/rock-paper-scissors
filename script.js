c05d51bf6c6319c91cd6f8bba79181909c8c5bad
Arturo <arturo9503@gmail.com>
2022-07-07 17:31:52 -0400

    Add DOM methods to display results

--- script.js
+++ script.js
@@ -31,11 +31,40 @@ function game(p1, pc){
     }
 }
 
+const container = document.querySelector('.container');
+let count = 1;
+let scorePC = 0;
+let scoreP1 = 0;
+
 window.addEventListener('click', function(e){
-    const p1 = e.target.innerText;
-    if (p1 == 'rock' || p1 == 'paper' || p1 == 'scissors'){
-        const pc = computerPlay();
-        console.log(game(p1, pc));
+    if (scoreP1 < 5 && scorePC < 5){
+        const p1 = e.target.innerText;
+        if (p1 == 'rock' || p1 == 'paper' || p1 == 'scissors'){
+            const pc = computerPlay();
+            const div = document.createElement('div');
+            const div2 = document.createElement('div');
+            let result = game(p1, pc);
+            if (result == 'Player wins!'){
+                scoreP1++;
+            }
+            else{
+                scorePC++;
+            }
+            div.textContent = "PC Picks: " + pc +' - Round '+ count + " "+result ;
+            div2.textContent = 'P1: ' + scoreP1 + " PC: " + scorePC;
+            div.style.cssText = "margin-top: 8px";
+            container.appendChild(div);
+            container.appendChild(div2);
+            count++;
+            if (scorePC == 5){
+                div.textContent = 'PC Wins Set!';
+                container.appendChild(div);
+            }
+            if (scoreP1 == 5){
+                div.textContent = 'P1 Wins Set!';
+                container.appendChild(div);
+            }
+        }
     }
 });
 
